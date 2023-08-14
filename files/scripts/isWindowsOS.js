1b3545ace5c601827f500bf83dcc6240f91a3f54
Lucas Gabriel (lucmsilva) <lucmsilva651@gmail.com>
2023-08-13 21:09:02 -0300

    Added website files

---
+++ files/scripts/isWindowsOS.js
@@ -0,0 +1,24 @@
+function isWindowsOS() {
+    return navigator.platform.indexOf('Win') > -1;
+}
+
+function showAlert() {
+    if (isWindowsOS()) {
+        
+    } else {
+        var alertDiv = document.createElement('div');
+        alertDiv.style.position = 'fixed';
+        alertDiv.style.top = '50%';
+        alertDiv.style.left = '50%';
+        alertDiv.style.transform = 'translate(-50%, -50%)';
+        alertDiv.style.padding = '20px';
+        alertDiv.style.background = '#f8d7da';
+        alertDiv.style.border = '1px solid #f5c6cb';
+        alertDiv.style.borderRadius = '4px';
+        alertDiv.style.textAlign = 'center';
+        alertDiv.innerHTML = '<p>ClassicAltTab is not supported in your system. Please use a Windows environment.</p>';
+        document.body.appendChild(alertDiv);
+    }
+}
+
+showAlert();
