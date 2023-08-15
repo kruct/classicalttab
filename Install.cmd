12e88a01766502135c27d9804d11475facee8a65
Lucas Gabriel (lucmsilva) <lucmsilva651@gmail.com>
2023-08-14 22:31:52 -0300

    Update Install.cmd

--- Install.cmd
+++ Install.cmd
@@ -2,7 +2,7 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 cd installerFiles
-echo Installing NTAltTab...
+echo Installing ClassicAltTab...
 echo.
 echo Made by Lucas Gabriel (lucmsilva)
 echo https://github.com/kruct/classicalttab
