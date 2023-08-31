736e8b17b52e95e540411f754b5041d45a6b75cb
Lucas Gabriel (lucmsilva) <lucmsilva651@gmail.com>
2023-08-30 21:09:09 -0300

    Update updaterCoreMain.cmd

--- updaterFiles/updaterCore/updaterCoreMain.cmd
+++ updaterFiles/updaterCore/updaterCoreMain.cmd
@@ -3,7 +3,8 @@
 title ydl-cli updater
 
 if "%1"=="" (
-    echo [ydl-cli updater] Use: %0 ^<URL^>
+    echo Use: %0 ^<URL^>
+    pause >nul
     exit /b 1
 )
 
@@ -14,9 +15,9 @@ certutil -urlcache -split -f "%url%" "%filename%" > nul
 
 if %errorlevel% equ 0 (
     echo.
-    echo [ydl-cli updater] Download complete: %filename%
-    echo [ydl-cli updater] Press any key to exit.
+    echo Download complete: %filename%
+    echo Press any key to exit.
     pause >nul
 ) else (
-    echo [ydl-cli updater] Download failed (we don't know why)
+    echo Download failed (we don't know why)
 )
